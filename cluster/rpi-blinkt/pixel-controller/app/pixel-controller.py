c581eb47441d28fcf9817c09424cd0ba03b3492c
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2020-10-19 21:04:50 +0000

    Add class and signal exit handlign for controlling resources

--- cluster/rpi-blinkt/pixel-controller/app/pixel-controller.py
+++ cluster/rpi-blinkt/pixel-controller/app/pixel-controller.py
@@ -3,40 +3,65 @@
 import time
 import blinkt
 import os
+import signal
 
 dir = "/home/pi/pixels/"
-BRIGHTNESS_1 = os.environ.get("BRIGHTNESS_1", 0)
-BRIGHTNESS_2 = os.environ.get("BRIGHTNESS_2", 0)
-BRIGHTNESS_3 = os.environ.get("BRIGHTNESS_3", 128)
-CLEAR_SLEEP = os.environ.get("CLEAR_SLEEP", 0.1)
 
-blinkt.set_clear_on_exit()
+class PixelController:
 
-while True:
-    no_pixels = 8
-    for i in range(0,8):
-        if os.path.isfile(dir + str(i)):
-            with open(dir + str(i), 'r') as f:
-                data = f.read()
-                try:
-                    br = data.split(':')
-                    BRIGHTNESS_1 = int(br[0])
-                    BRIGHTNESS_2 = int(br[1])
-                    BRIGHTNESS_3 = int(br[2])
-                except:
-                    pass
-            print('Set pixel {}: {} {} {}'.format(i, BRIGHTNESS_1, BRIGHTNESS_2, BRIGHTNESS_3))
-            blinkt.set_pixel(i, BRIGHTNESS_1, BRIGHTNESS_2, BRIGHTNESS_3)
-            no_pixels += 1
-        else:
-            blinkt.set_pixel(i, 0, 0, 0)
-            no_pixels -= 1
+    def __init__(self):
+        self.kill = False
 
-    blinkt.show()
-    time.sleep(float(CLEAR_SLEEP))
+        self.signals = {
+            signal.SIGINT: 'SIGINT',
+            signal.SIGTERM: 'SIGTERM'
+        }
 
-    if no_pixels == 0:
-        for i in range(0,8):
-            blinkt.clear()
-            blinkt.set_pixel(i, 0, 0, 0)
-        blinkt.show()
+        self.BRIGHTNESS_1 = os.environ.get("BRIGHTNESS_1", 0)
+        self.BRIGHTNESS_2 = os.environ.get("BRIGHTNESS_2", 0)
+        self.BRIGHTNESS_3 = os.environ.get("BRIGHTNESS_3", 128)
+        self.CLEAR_SLEEP = os.environ.get("CLEAR_SLEEP", 0.1)
+
+        blinkt.set_clear_on_exit()
+        
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def run(self):
+        while not self.kill:
+            no_pixels = 8
+            for i in range(0,8):
+                if os.path.isfile(dir + str(i)):
+                    with open(dir + str(i), 'r') as f:
+                        data = f.read()
+                        try:
+                            br = data.split(':')
+                            self.BRIGHTNESS_1 = int(br[0])
+                            self.BRIGHTNESS_2 = int(br[1])
+                            self.BRIGHTNESS_3 = int(br[2])
+                        except:
+                            pass
+                    print('Set pixel {}: {} {} {}'.format(i, self.BRIGHTNESS_1, self.BRIGHTNESS_2, self.BRIGHTNESS_3))
+                    blinkt.set_pixel(i, self.BRIGHTNESS_1, self.BRIGHTNESS_2, self.BRIGHTNESS_3)
+                    no_pixels += 1
+                else:
+                    blinkt.set_pixel(i, 0, 0, 0)
+                    no_pixels -= 1
+
+            blinkt.show()
+            time.sleep(float(self.CLEAR_SLEEP))
+
+            if no_pixels == 0:
+                for i in range(0,8):
+                    blinkt.clear()
+                    blinkt.set_pixel(i, 0, 0, 0)
+                blinkt.show()
+
+    def exit_gracefully(self, signum, frame):
+        print("\nReceived {} signal".format(self.signals[signum]))
+        self.kill = True
+
+if __name__ == '__main__':
+    pk = PixelController()
+    pk.run()
+
--- cluster/rpi-blinkt/single-pixel/app/single-pixel.py
+++ cluster/rpi-blinkt/single-pixel/app/single-pixel.py
@@ -3,30 +3,55 @@
 
 import time
 import os
+import signal
 
 dir = "/home/pi/pixels/"
-cf = ""
-SLEEP = int(os.environ.get("SLEEP",60))
-BRIGHTNESS_1 = int(os.environ.get("BRIGHTNESS_1", 0))
-BRIGHTNESS_2 = int(os.environ.get("BRIGHTNESS_2", 0))
-BRIGHTNESS_3 = int(os.environ.get("BRIGHTNESS_3", 128))
 
-print('Brightness: {} {} {}, Sleep: {}'.format(BRIGHTNESS_1, BRIGHTNESS_2, BRIGHTNESS_3, SLEEP))
+class SinglePixel:
 
-for i in range(8):
-    print(i)
-    if(i == 8):                                     # If i == 8, the end has been reached and it should not try to enable anymore leds
-        print("No more spots left")
-        break
-    elif not os.path.isfile(dir + str(i)):          # If the file is not there, create it, so the pixel controller will know to turn on the pixel
-        print("Pixel ",i," available, activating!")
-        cf = dir + str(i)
-        with open(cf, 'w') as f:
-            f.write('{}:{}:{}'.format(BRIGHTNESS_1, BRIGHTNESS_2, BRIGHTNESS_3))
-        break
+    def __init__(self):
+        self.kill = False
 
-try:
-    time.sleep(SLEEP)
-finally:
-    if cf != "":
-        os.remove(cf)
+        self.signals = {
+            signal.SIGINT: 'SIGINT',
+            signal.SIGTERM: 'SIGTERM'
+        }
+
+        self.cf = ""
+        self.BRIGHTNESS_1 = int(os.environ.get("BRIGHTNESS_1", 0))
+        self.BRIGHTNESS_2 = int(os.environ.get("BRIGHTNESS_2", 0))
+        self.BRIGHTNESS_3 = int(os.environ.get("BRIGHTNESS_3", 128))
+
+        print('Brightness: {} {} {}'.format(self.BRIGHTNESS_1, self.BRIGHTNESS_2, self.BRIGHTNESS_3))
+
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def run(self):
+        for i in range(8):
+            print(i)
+            if(i == 8):                                     # If i == 8, the end has been reached and it should not try to enable anymore leds
+                print("No more spots left")
+                break
+            elif not os.path.isfile(dir + str(i)):          # If the file is not there, create it, so the pixel controller will know to turn on the pixel
+                print("Pixel ",i," available, activating!")
+                self.cf = dir + str(i)
+                with open(self.cf, 'w') as f:
+                    f.write('{}:{}:{}'.format(self.BRIGHTNESS_1, self.BRIGHTNESS_2, self.BRIGHTNESS_3))
+                break
+
+    def exit_gracefully(self, signum, frame):
+        print("\nReceived {} signal".format(self.signals[signum]))
+
+        if self.cf != "":
+            os.remove(self.cf)
+
+        self.kill = True
+
+if __name__ == '__main__':
+    pk = SinglePixel()
+    pk.run()
+
+    while not pk.kill:
+        time.sleep(1)
+
