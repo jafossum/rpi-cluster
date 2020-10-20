27c9cfb9052048e137f23f650149f3fb4137316a
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2020-10-20 18:32:40 +0000

    Build directly with correct name

--- cluster/rpi-blinkt/pixel-controller/build.sh
+++ cluster/rpi-blinkt/pixel-controller/build.sh
@@ -1,8 +1,5 @@
 # Build image
-docker build . -t pixel-controller
-
-# Tag image
-docker tag pixel-controller jafossum/blinkt-pixel-controller
+docker build . -t jafossum/blinkt-pixel-controller
 
 # Push image
 docker push jafossum/blinkt-pixel-controller
--- cluster/rpi-blinkt/single-pixel/build.sh
+++ cluster/rpi-blinkt/single-pixel/build.sh
@@ -1,8 +1,5 @@
 # Build image
-docker build . -t single-pixel
-
-# Tag image
-docker tag single-pixel jafossum/blinkt-single-pixel
+docker build . -t jafossum/blinkt-single-pixel
 
 # Push image
 docker push jafossum/blinkt-single-pixel
