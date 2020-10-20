4777e99860b9ddfff098ff6de756cf1e733088bf
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2020-10-20 23:39:55 +0200

    Pass filename

--- cluster/rpi-web-mongo/web-app/app/main.go
+++ cluster/rpi-web-mongo/web-app/app/main.go
@@ -15,47 +15,43 @@ const (
 	dir = "/home/pi/pixels/"
 )
 
-type singlePix struct {
-	fi string
-}
-
 func main() {
 	// Init rand generator
 	rand.Seed(time.Now().UnixNano())
 
 	log.Println("Starting")
-	sp := singlePix{}
 
-	http.HandleFunc("/blinkt", sp.handler)
+	http.HandleFunc("/blinkt", handler)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
-func (sp *singlePix) handler(w http.ResponseWriter, r *http.Request) {
-	sp.createFile()
-	defer sp.deleteFile()
-	fmt.Fprintf(w, "Hello! File: "+sp.fi)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fi := createFile()
+	defer deleteFile(fi)
+	fmt.Fprintf(w, "Hello! File: "+fi)
 	time.Sleep(time.Second)
 }
 
-func (sp *singlePix) deleteFile() {
+func deleteFile(fi string) {
 	log.Println("- Run Clean Up - Delete File")
-	_ = os.Remove(dir + sp.fi)
+	_ = os.Remove(dir + fi)
 	log.Println("- Good bye!")
 }
 
-func (sp *singlePix) createFile() {
+func createFile() string {
 	log.Println("- Create File")
 
+	fi := ""
 	i := rand.Intn(8)
 	log.Println(i)
 
 	if _, err := os.Stat(dir + strconv.Itoa(i)); os.IsNotExist(err) {
 		dp := []byte(fmt.Sprintf("%d:%d:%d", rand.Intn(255), rand.Intn(255), rand.Intn(255)))
-		sp.fi = strconv.Itoa(i)
-		if err := ioutil.WriteFile(dir+sp.fi, dp, 0666); err != nil {
+		fi = strconv.Itoa(i)
+		if err := ioutil.WriteFile(dir+fi, dp, 0666); err != nil {
 			log.Println(err)
 			os.Exit(3)
 		}
 	}
+	return fi
 }
-
